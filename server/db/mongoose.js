642dd9e04187f24a55df973eae8c0de99101c4dc
nathalie1107 <nathalie.nhu.y@gmail.com>
2018-08-22 13:35:29 +0800

    setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+//mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://nathalie:080192@ds125362.mlab.com:25362/todo-app-1111');
 
 module.exports = {mongoose};
