534bb3d7a4845951199225d6f93c09ff561e3a58
nathalie1107 <nathalie.nhu.y@gmail.com>
2018-08-22 13:51:20 +0800

    MONGODB_URI setting

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-//mongoose.connect('mongodb://localhost:27017/TodoApp');
-mongoose.connect('mongodb://nathalie:080192@ds125362.mlab.com:25362/todo-app-1111');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+// mongoose.connect('mongodb://nathalie:080192@ds125362.mlab.com:25362/todo-app-1111');
 
 module.exports = {mongoose};
