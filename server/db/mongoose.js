841de3b3bf4496a956ba81b7fcb493072e35381e
nathalie1107 <nathalie.nhu.y@gmail.com>
2018-08-22 21:32:53 +0800

    edit mongodb

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,14 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
-mongoose.connect('mongodb://nathalie:080192@ds125362.mlab.com:25362/todo-app-1111');
+var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
+                replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };       
+ 
+var mongodbUri = 'mongodb://saphira:happy2018@ds125362.mlab.com:25362/todo-app-1111';
+ 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', options);
+
+//mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+//mongoose.connect('mongodb://nathalie1107:nhuylatoi1107@ds125362.mlab.com:25362/todo-app-1111');
 
 module.exports = {mongoose};
