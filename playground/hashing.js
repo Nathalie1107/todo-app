4f2ab3ce80da0a7577a3da9a1e9f1bc30eed4b4d
nathalie1107 <nathalie.nhu.y@gmail.com>
2018-08-24 10:22:13 +0800

    L92 - Hashing Password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,34 +1,48 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-}
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// bcrypt.genSalt(12, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     })
+// } )
 
-var decoded = jwt.verify(token, '123abc');
-console.log(decoded);
+var hashedPassword = '$2a$12$C0kVhcW8EQOScnfoROjOW.aa3Lwne3KSQjgvcPa8EE3DiHqC9SJTq';
+bcrypt.compare(password, hashedPassword, (err, success) => {
+    console.log(success);
+})
 
-var message = 'adminpw';
-var hash = SHA256(message).toString();
-console.log(`Message: ${message}`);
-console.log(`Hash: ${hash}`);
+// var data = {
+//     id: 10
+// }
 
-var data = {
-    id: 4
-};
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
 
-var token = {
-    data,
-    hash: SHA256(JSON.stringify(data) + 'some secret').toString()
-}
+// var decoded = jwt.verify(token, '123abc');
+// console.log(decoded);
 
-var resultHash = SHA256(JSON.stringify(token.data) + 'some secrete').toString();
+// var message = 'adminpw';
+// var hash = SHA256(message).toString();
+// console.log(`Message: ${message}`);
+// console.log(`Hash: ${hash}`);
 
-if (resultHash === token.hash){
-    console.log('Data was not changed');
-} else {
-    console.log('Data was changed. Don\'t trust it');
-}
+// var data = {
+//     id: 4
+// };
+
+// var token = {
+//     data,
+//     hash: SHA256(JSON.stringify(data) + 'some secret').toString()
+// }
+
+// var resultHash = SHA256(JSON.stringify(token.data) + 'some secrete').toString();
+
+// if (resultHash === token.hash){
+//     console.log('Data was not changed');
+// } else {
+//     console.log('Data was changed. Don\'t trust it');
+// }
