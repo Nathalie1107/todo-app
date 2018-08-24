c5b7cdb4ea9433cb4a793142ee17c353e241c814
nathalie1107 <nathalie.nhu.y@gmail.com>
2018-08-24 16:56:43 +0800

    delete dupplicated POST users/ and test POST users/ added

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ if(env==='development'){
 
 
 const _ = require('lodash');
-var express = require('express');
+var express = require('express'); 
 var bodyParser = require('body-parser'); 
 var {ObjectID} = require('mongodb');
 
@@ -107,18 +107,18 @@ app.patch('/todos/:id', (req, res) => {
   })
 })
 
-app.post('/users', (req, res) => {
-  var body = _.pick(req.body, ['email', 'password']);
-  var user = new User(body);
+// app.post('/users', (req, res) => {
+//   var body = _.pick(req.body, ['email', 'password']);
+//   var user = new User(body);
 
-  user.save().then(() => {
-    return user.generateAuthToken();
-  }).then(token => {
-    res.header('x-auth', token).send(user);
-  }).catch(e => {
-    res.status(401).send(e);
-  })
-})
+//   user.save().then(() => {
+//     return user.generateAuthToken();
+//   }).then(token => {
+//     res.header('x-auth', token).send(user);
+//   }).catch(e => {
+//     res.status(401).send(e);
+//   })
+// })
 
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
@@ -132,8 +132,6 @@ app.post('/users', (req, res) => {
     res.status(400).send(e);
   })
 });
-
-
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
