d3cc2aaf975be7041908eff273c2d9b0782eb08a
nathalie1107 <nathalie.nhu.y@gmail.com>
2018-08-24 09:38:45 +0800

    L91 - Private routes and Auth Middleware

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,7 +5,7 @@ mongoose.Promise = global.Promise;
 var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
                 replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };       
  
-var mongodbUri = 'mongodb://saphira:happy2018@ds125362.mlab.com:25362/todo-app-1111';
+//var mongodbUri = 'mongodb://saphira:happy2018@ds125362.mlab.com:25362/todo-app-1111';
  
 mongoose.connect(process.env.MONGODB_URI, options);
 
